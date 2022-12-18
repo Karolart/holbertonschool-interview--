4324b32b3cd50d38ffc0d0a920386e6a80ba54b7
Karolart <karoljuzz1610@gmail.com>
2022-12-18 15:29:50 -0500

    update file

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
-"""
-Given a number n, write a method that calculates the fewest number
-of operations needed to result in exactly n H characters in the file.
-"""
+"""Minimum Operations"""
 
 
-def countProcess(num):
-    """ Return list of process until n H """
-     con = 2
+def Factorization(x):
+    """Returns elements of x"""
+    div = 2
     array = list()
-    while (con <= x):
-        if x % con == 0:
-            array.append(con)
-            x /= con
+    while (div <= x):
+        if x % div == 0:
+            array.append(div)
+            x /= div
         else:
-            con += 1
+            div += 1
+
     return array
 
 
-
 def minOperations(n):
-    """ Return sum of process until n H """
-     min = 0
-    factors = [x for x in countProcess(n)]
-    matter = {item: factors.count(item) for item in factors}
-    for k, v in matter.items():
+    """Calculates the fewest number of operations needed
+        to result in exactly n H characters in the file"""
+    min = 0
+    factors = [x for x in Factorization(n)]
+    occurences = {item: factors.count(item) for item in factors}
+    for k, v in occurences.items():
         min += k * v
     return min
